c0f0e0333bcb1b8361c2ead7d3fff5147eb11a3d
Nalrarang <nalrarang@gmail.com>
2017-12-28 16:16:58 +0900

    모달 플러그인 완성

--- js/customModal.js
+++ js/customModal.js
@@ -1,26 +1,33 @@
+/*
+*  jQuery Modal Plugin
+*  설명: jQuery용 CustomModal플러그인입니다.
+        import 하기전에 jquery를 먼저 Import해주시길 바랍니다.
+*  작성자: Nalrarang
+*  작성일: 2017.12.28
+*/
+
 (function ($) {
     $.fn.customModal = function (options) {
         var modal = $(this);
         var body = $('body');
+        /* Modal 기본 옵션 */
         var modalOptions = $.extend({
             id: 'modalContent_' + new Date().getTime(),
             position: 'center',
             width: '420',
             height: '220',
+            close: true,
             left: '0',
             top: '0',
-            multiple: true,
-            zindex: 1,
-            template: '<p class="modal-text">So implement your design and place content here! If you want to close modal, please hit "Esc", click somewhere on the screen or use special button.</p>',
+            multiple: false,
+            template: '<p class="modal-text">국회의원은 국가이익을 우선하여 양심에 따라 직무를 행한다.</p><p class="modal-text">대통령은 국가의 독립·영토의 보전·국가의 계속성과 헌법을 수호할 책무를 진다.</p><p class="modal-text">훈장등의 영전은 이를 받은 자에게만 효력이 있고, 어떠한 특권도 이에 따르지 아니한다.</p>',
             onInit: function () { },
             onBeforeShow: function () { },
             onAfterHide: function () { },
         }, options);
 
         var _init = function () {
-            /* Init 할때 먼저 실행해야 할게 있으면 해준다. */
-            modalOptions.onInit();
-
+            /* 모달의 위치를 가운데로 하기 위한 계산 */
             var _windowWidth = window.innerWidth;
             var _windowHeight = window.innerHeight;
 
@@ -36,18 +43,27 @@
                 'background': 'white',
                 'overflow': 'auto'
             }
-            var modalTemplate = '<div id="' + modalOptions.id + '" class="modal" data-multiple=' + modalOptions.multiple + '><div class="modal-bg"></div><div class="modal-box cs-modal">' + modalOptions.template + modalOptions.template + modalOptions.template + modalOptions.template + '</div></div>';
+            var modalTemplate = '<div id="' + modalOptions.id + '" class="modal" data-multiple=' + modalOptions.multiple + '><div class="modal-bg"></div><div class="modal-box">';
+            /* close옵션에 따라 닫기버튼을 추가해준다. */
+            if(modalOptions.close) {
+                modalTemplate += '<a href="#" class="modal-close">X</a>';                
+            }
+            modalTemplate += '<div class="cs-modal">' + modalOptions.template + '</div></div></div>';        
+
+            /* 만들어진 Template을 Body에 Append */
             body.append(modalTemplate);
             var elemObj = $('#' + modalOptions.id);
             var modalContent = elemObj.children('.modal-box');
             modalContent.css(styles);
 
+            /* 해당 Modal의 Callback Event를 추가해준다. */
             elemObj.on('beforeShow', function () {
                 modalOptions.onBeforeShow();
             });
             elemObj.on('afterHide', function () {
                 modalOptions.onAfterHide();
             });
+
             /* Background Click 속성이 True면 Overlay클릭시 모달 닫힘 */
             if (modal.attr('data-modal-bgclick')) {
                 elemObj.find('.modal-bg').on('click', function () {
@@ -59,8 +75,15 @@
                 evt.preventDefault();
                 _show(elemObj);
             });
+            elemObj.find('.modal-close').off('click').on('click', function (evt) {
+                evt.preventDefault();
+                _hide(elemObj);
+            });
+            /* Init후 실행해야 할게 있으면 해준다. */
+            modalOptions.onInit();
         }
 
+        /* Modal을 보여준다. */
         var _show = function (modalContent) {
             /* Modal이 Show되기전에 호출할게 있으면 먼저 해줌. */
             var events = $._data(modalContent[0], 'events');
@@ -68,15 +91,18 @@
                 events.beforeShow[events['beforeShow'].length - 1].handler();
             }
 
-            /* */
+            /* Multiple옵션에 따라 이전에 있던 모달을 닫을지 결정한다. */
             if (modalContent.attr('data-multiple') == 'false') {
                 $('.modal').hide();
+            } else {
+                body.append(modalContent);
             }
 
             body.addClass('modal-open');
             modalContent.children('.modal-box').removeClass('modal-out').addClass('styled-pane');
             modalContent.show();
         }
+        /* Modal을 감춘다. */
         var _hide = function (modalContent) {
             body.removeClass('modal-open');
             modalContent.children('.modal-box').removeClass('styled-pane').addClass('modal-out');
@@ -90,12 +116,14 @@
             }, 300);
         }
 
+        /* Modal과 관련된 요소를 삭제한다. */
         var _destroy = function (modalContent) {
             modalContent.off('beforeShow');
             modalContent.off('afterHide');
             modalContent.remove();
         }
 
+        /* CustomModal의 Method 모음 */
         var methods = {
             init: function () {
                 _init();
@@ -111,6 +139,7 @@
             }
         }
 
+        /* Modal Init할때와 Method호출할때의 상태에 따라 다른 행동을 함 */
         if (methods[options]) {
             methods[options]();
         } else {
